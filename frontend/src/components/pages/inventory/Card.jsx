33f535cf717af8e70ef909203c46eee0b0a3e0f6
JavierIvanValenzuelaEsparza <0321101377@ut-tijuana.edu.mx>
2024-06-10 15:51:37 -0700

    updateModal Images

--- frontend/src/components/pages/inventory/Card.jsx
+++ frontend/src/components/pages/inventory/Card.jsx
@@ -7,16 +7,18 @@ import { FormGroup } from '../../../Styled/ModalStyled';
 import { Labels } from '../../../Styled/Global.styled';
 
 axios.defaults.baseURL = 'https://smartpipes.cloud/api/inventory/';
-axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE4MDYzMjM1LCJpYXQiOjE3MTc5NzY4MzUsImp0aSI6IjJiYzk2Y2FiZTJkYzRjMzdhYzg2ZmJmZDg5NmZhNzA0IiwidXNlcl9pZCI6Mn0.Nl8Fgwjl3V8Y5kG39S_d0T9y-LEDHxBObrQ3gR1tpUo';
+axios.defaults.headers.common['Authorization'] = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE4MTQxMzUwLCJpYXQiOjE3MTgwNTQ5NTAsImp0aSI6Ijk4Y2Y4MGI1ZTZiMDQ4MDhiMTkwMGNmMzU0MmM1NTYzIiwidXNlcl9pZCI6Mn0.2bSoFio-nxblDwmPmVPEjmXOUEubh_j-r0z4P62vDC0';
 
 const Card = ({ inventory_id, item_id, item_name, image_icon, item_price, stock, item_type, children }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [cardTitle, setCardTitle] = useState(item_name);
     const [cardPrice, setCardPrice] = useState(item_price);
     const [cardStock, setCardStock] = useState(stock);
+    const [cardImage, setCardImage] = useState(image_icon);
     const [rawMaterialSuppliers, setRawMaterialSuppliers] = useState([]);
     const [products, setProducts] = useState([]);
     const [rawMaterials, setRawMaterials] = useState([]);
+    const [selectedImage, setSelectedImage] = useState(null);
     const [error, setError] = useState(null);
 
     useEffect(() => {
@@ -30,7 +32,7 @@ const Card = ({ inventory_id, item_id, item_name, image_icon, item_price, stock,
                 setProducts(productsResponse.data);
                 setRawMaterials(rawMaterialsResponse.data);
             } catch (err) {
-                console.error('Error fetching data:', err);
+                console.error('Error al obtener datos:', err);
                 setError(err);
             }
         };
@@ -48,11 +50,7 @@ const Card = ({ inventory_id, item_id, item_name, image_icon, item_price, stock,
     const handleTitleChange = (e) => {
         const newTitle = e.target.value;
         setCardTitle(newTitle);
-        if (item_type === 'RawMaterial') {
-            saveChanges({ name: newTitle, description: "Descripción actualizada", category: 1 }, 'raw-materials', item_id);
-        } else {
-            saveChanges({ name: newTitle, description: "Descripción actualizada", price: cardPrice, status: "Active" }, 'products', item_id);
-        }
+        updateData({ name: newTitle });
     };
 
     const handlePriceChange = (e) => {
@@ -61,25 +59,25 @@ const Card = ({ inventory_id, item_id, item_name, image_icon, item_price, stock,
             return;
         }
         setCardPrice(newPrice);
-    
+
         if (item_type === 'RawMaterial') {
             const rawMaterial = rawMaterials.find(rm => rm.raw_material_id === item_id);
             if (!rawMaterial) {
-                console.error('Raw material not found for item_id:', item_id);
+                console.error('Materia prima no encontrada para item_id:', item_id);
                 return;
             }
             const supplier = rawMaterialSuppliers.find(s => s.raw_material === rawMaterial.raw_material_id);
             if (!supplier) {
-                console.error('Supplier not found for raw_material_id:', rawMaterial.raw_material_id);
+                console.error('Proveedor no encontrado para raw_material_id:', rawMaterial.raw_material_id);
                 return;
             }
-    
+
             const rawMaterialData = {
                 purchase_price: newPrice.toString(),
                 raw_material: rawMaterial.raw_material_id,
                 supplier: supplier.supplier // Ajustar este campo si necesario
             };
-    
+
             console.log('Datos enviados para raw-material-suppliers:', rawMaterialData);
             saveChanges(rawMaterialData, 'raw-material-suppliers', supplier.id);
         } else {
@@ -93,7 +91,6 @@ const Card = ({ inventory_id, item_id, item_name, image_icon, item_price, stock,
             saveChanges(productData, 'products', item_id);
         }
     };
-    
 
     const handleStockChange = (e) => {
         const newStock = parseInt(e.target.value, 10);
@@ -101,17 +98,55 @@ const Card = ({ inventory_id, item_id, item_name, image_icon, item_price, stock,
             return;
         }
         setCardStock(newStock);
-    
+
         const inventoryData = {
             item_id: item_id,
             item_type: item_type,
             stock: newStock
         };
-    
+
         console.log('Datos enviados para inventory:', inventoryData);
         saveChanges(inventoryData, 'inventory', inventory_id);
     };
-    
+
+    const handleImageChange = (e) => {
+        const image = e.target.files[0];
+        setSelectedImage(image);
+    };
+
+    useEffect(() => {
+        if (selectedImage !== null) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setCardImage(reader.result);
+                updateData({ image_icon: selectedImage });
+            };
+            reader.readAsDataURL(selectedImage);
+        }
+    }, [selectedImage]);
+
+    const updateData = (data, updateType = '', value = null) => {
+        const requestData = {
+            name: cardTitle,
+            description: "Descripción actualizada",
+            category: 1,
+            price: cardPrice,
+            status: "Active",
+            ...data
+        };
+
+        if (item_type === 'RawMaterial') {
+            requestData.purchase_price = cardPrice.toString();
+        }
+
+        if (updateType === 'price') {
+            requestData.price = value;
+        }
+
+        console.log('Updating data:', requestData); // Added for debugging
+
+        saveChanges(requestData, item_type === 'RawMaterial' ? 'raw-materials' : 'products', item_id);
+    };
 
     const saveChanges = async (data, type, id) => {
         try {
@@ -133,12 +168,34 @@ const Card = ({ inventory_id, item_id, item_name, image_icon, item_price, stock,
                     url = `inventory/${id}/`;
                     break;
             }
-            console.log('URL:', url);
-            console.log('Data:', data);
-            const response = await axios.put(url, data);
-            console.log('Respuesta del servidor:', response.data);
+
+            const formData = new FormData();
+            Object.keys(data).forEach(key => {
+                formData.append(key, data[key]);
+            });
+
+            if (data.image_icon) {
+                formData.append('image_icon', data.image_icon);
+            }
+
+            console.log('FormData being sent:', formData); // Added for debugging
+
+            const response = await axios.put(url, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                }
+            });
+
+            console.log('Respuesta PUT:', response.data);
+
+            // Realizar una solicitud GET para verificar si los datos se han actualizado
+            const getResponse = await axios.get(url);
+            console.log('Respuesta GET:', getResponse.data);
         } catch (err) {
-            console.error('Error al guardar cambios:', err.response ? err.response.data : err.message);
+            console.error('Error al guardar cambios:', err.response ? err.response.data : err);
+            console.error('Estado de la respuesta:', err.response ? err.response.status : 'No disponible');
+            console.error('Encabezados de la respuesta:', err.response ? err.response.headers : 'No disponible');
+            console.error('Error completo:', err);
         }
     };
 
@@ -153,7 +210,7 @@ const Card = ({ inventory_id, item_id, item_name, image_icon, item_price, stock,
                     <p>Disponible: {cardStock} Unidades</p>
                     {children}
                 </CardBody>
-                <CardImage src={image_icon} alt={cardTitle} />
+                <CardImage src={cardImage} alt={cardTitle} />
             </InventoryCard>
             <UpdateModal isOpen={isModalOpen} onClose={handleCloseModal}>
                 <form>
@@ -176,15 +233,22 @@ const Card = ({ inventory_id, item_id, item_name, image_icon, item_price, stock,
                         />
                     </FormGroup>
                     <FormGroup>
-                        <Labels>Existencia</Labels>
+                        <Labels>Stock</Labels>
                         <input
                             type="number"
                             value={cardStock}
                             onChange={handleStockChange}
-                            placeholder="Ingresa la nueva existencia"
+                            placeholder="Ingresa el nuevo stock"
                         />
                     </FormGroup>
-                    {children}
+                    <FormGroup>
+                        <Labels>Imagen</Labels>
+                        <input
+                            type="file"
+                            onChange={handleImageChange}
+                            placeholder="Selecciona una nueva imagen"
+                        />
+                    </FormGroup>
                 </form>
             </UpdateModal>
         </>
@@ -199,7 +263,7 @@ Card.propTypes = {
     item_price: PropTypes.number.isRequired,
     stock: PropTypes.number.isRequired,
     item_type: PropTypes.string.isRequired,
-    children: PropTypes.node,
+    children: PropTypes.node
 };
 
 export default Card;
